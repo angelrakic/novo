b10280997ba882a08c9dc732a2342b437ad357d2
angelrakic <andjela.rakic@orioninc.com>
2021-09-07 14:46:59 +0200

    Initial Commit

---
+++ CollectionViewBurger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewBurger
+//
+//  Created by Andjela Rakic on 9/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
